e4979f693537486a3cbdb50947d1c1116664d331
Your Name <mseif1719@gmail.com>
2026-02-09 12:29:49 +0200

    Delete Run

--- PersonApi/obj/Debug/net9.0/PersonApi.AssemblyInfo.cs
+++ PersonApi/obj/Debug/net9.0/PersonApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710847a274ed6db0f4307d6e8e9fbcbfc457852b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
